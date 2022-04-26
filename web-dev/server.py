f009d7c78cf0fbb3c90d9e568984611094c12a5b
Jonathan Olmon <jonathan.olmon@gmail.com>
2022-04-26 15:26:27 -0500

    Got the server up and running with a home page for the portfollio.

--- web-dev/server.py
+++ web-dev/server.py
@@ -3,9 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 print(__name__)
 
-@app.route("/<username>")
-def hello_world(username=None):
-    return render_template('index.html', username=username)
+@app.route("/")
+def my_home():
+    return render_template('index.html')
 
 @app.route("/about")
 def about():
